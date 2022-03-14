0198dcee7a9a59a9f59403581601ce6391bc5e3f
avi98 <9958703925.dad@gmail.com>
2022-03-14 22:26:07 +0530

    feat: file path and installing the pref report

--- pref-monitor-ci/src/prefReport.ts
+++ pref-monitor-ci/src/prefReport.ts
@@ -2,21 +2,24 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 import * as task from "azure-pipelines-task-lib/task";
+import { variables } from "./utils/variables";
 
 const { v4: uuid4 } = require("uuid");
 
 export class PrefReportCI {
   #prefMonitor: string = "pref-report-cli";
-  #commentFilePath: string = "";
+  #configFilePath: string = "";
 
   constructor() {
-    this.#commentFilePath = path.join(
-      task.getVariable("Build.SourcesDirectory") || "",
-      "/comment.txt"
-    );
+    this.#configFilePath =
+      variables.Env.Params.ConfigFile ||
+      path.join(process.cwd(), "webVitalsrc.js");
   }
 
   async run() {
+    const configFile = this.#configFilePath;
+
+    task.debug(configFile);
     const prefMonitor = this.#prefMonitor;
     const isInstalledPrefMonitor = task.which(prefMonitor);
 
@@ -35,40 +38,43 @@ export class PrefReportCI {
   }
 
   async runPrefMonitor() {
-    const prefTool = require(this.#prefMonitor);
-
-    await task.tool(prefTool);
-    prefTool
-      .line("-r")
+    const prefTool = this.#prefMonitor;
+    const hasPrefTool = task.which(prefTool);
+    task.debug(`prefTool --configFilePath ${this.#configFilePath}`);
+    console.log("file path", this.#configFilePath);
+    if (!hasPrefTool) return;
+    await task
+      .tool(prefTool)
+      .line("--markdown")
+      .line(`--configFilePath ${this.#configFilePath}`)
       .exec()
       .then(() => {
         task.debug(`-------Completed running the ${this.#prefMonitor}------`);
-        this.readComment();
       })
       .catch((error: any) => {
         task.setResult(task.TaskResult.Failed, error);
       });
   }
 
-  private readComment() {
-    try {
-      const filePath = this.#commentFilePath;
-      const data = fs.readFileSync(filePath);
-      console.log("data", data);
-    } catch (e) {}
-  }
+  // private readComment() {
+  //   try {
+  //     const filePath = this.#commentFilePath;
+  //     const data = fs.readFileSync(filePath);
+  //     console.log("data", data);
+  //   } catch (e) {}
+  // }
 
   private async installPrefMonitor() {
     try {
       const tempDir = task.getVariable("agent.tempDirectory") || process.cwd();
       task.checkPath(tempDir, `${tempDir} ${tempDir}`);
-      const filePath = path.join(tempDir, uuid4() + ".sh");
+      const filePath = path.join(process.cwd(), uuid4() + ".sh");
       if (os.platform() !== "win32")
         fs.writeFileSync(
           filePath,
           `sudo PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install -g ${
             this.#prefMonitor
-          }`,
+          } --unsafe-perm=true --allow-root`,
           { encoding: "utf8" }
         );
 
---
+++ pref-monitor-ci/src/utils/variables.ts
@@ -0,0 +1,35 @@
+import * as task from "azure-pipelines-task-lib/task";
+
+export const variables = {
+  //----env
+  Env: {
+    //----params
+    Params: {
+      ConfigFile: task.getInput("CONFIGFILEPATH"),
+    },
+    Agent: {
+      JobStatus: getVariable("AGENT_JOB_STATUS"),
+      Name: getVariable("AGENT_NAME"),
+      TempDir: getVariable("AGENT_TEMPDIRECTORY"),
+    },
+    System: {
+      AccessToken: getVariable("SYSTEM_ACCESSTOKEN"),
+      DefinitionName: getVariable("SYSTEM_DEFINITIONNAME"),
+      TeamFoundationServerUri: getVariable("SYSTEM_TEAMFOUNDATIONSERVERURI"),
+      TeamProject: getVariable("SYSTEM_TEAMPROJECT"),
+      SourceDir: getVariable("BUILD_SOURCE_DIRECTORY"),
+    },
+
+    //---- debug
+    Debug: {
+      Pat: getVariable("DEBUG_PAT"),
+    },
+  },
+};
+
+function getVariable(name: string) {
+  const v = task.getVariable(name);
+  if (!v) return "";
+
+  return v;
+}
