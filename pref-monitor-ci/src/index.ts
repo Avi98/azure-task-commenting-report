932e18da67a0c92b5ebf1c8b4aba7de9ace7bb05
avi98 <9958703925.dad@gmail.com>
2022-05-15 13:18:10 +0530

    fix: instance created at root file

--- pref-monitor-ci/src/index.ts
+++ pref-monitor-ci/src/index.ts
@@ -1,9 +1,12 @@
 import * as task from "azure-pipelines-task-lib/task";
-import { commentLHReport } from "./createPrComment";
+import { CreatePRComment } from "./createPrComment";
 import { PrefReportCI } from "./prefReport";
 
 const exec = new PrefReportCI();
 exec
   .run()
-  .then(() => commentLHReport.commentReport())
+  .then(() => {
+    const commentLHReport = new CreatePRComment();
+    commentLHReport.commentReport();
+  })
   .catch((error) => task.setResult(task.TaskResult.Failed, error));
